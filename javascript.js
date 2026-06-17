410c239aa29834d9f6fbe41a5b9d60b1da6541f1
splossion <124319437+splossion@users.noreply.github.com>
2026-06-16 19:51:14 -0600

    add color

--- javascript.js
+++ javascript.js
@@ -9,3 +9,27 @@ for (let i = 0; i < 16; i++){
         newDiv.append(newGrid);
     }
 }
+
+const grid = document.querySelectorAll(".grid");
+
+grid.forEach(grid => {
+    grid.dataset.visits = "0";
+    grid.addEventListener("mouseover", (e) => {
+        let visits = Number(grid.dataset.visits);
+        if (visits == "0") grid.style.backgroundColor = getRandomColor();
+        
+        visits = Math.min(visits + 0.1, 1);
+        grid.dataset.visits = visits;
+        grid.style.opacity = visits;
+    });
+
+});
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
